019d4d252a4dd9a4475c3cf739617a2949d234fb
jjh946 <asdfg3128@gmail.com>
2023-03-28 17:46:14 +0900

    최소 힙

--- 조준형/heap공부.py
+++ 조준형/heap공부.py
@@ -23,3 +23,14 @@ result = heapq.heappop(heap)
 
 print(result)
 print(heap)
+
+
+#최대 힙 만들기
+heap_items = [1,3,5,7,9]
+
+max_heap = []
+for item in heap_items:
+  heapq.heappush(max_heap, (-item, item))
+
+print(max_heap)
+#[(-9, 9), (-7, 7), (-3, 3), (-1, 1), (-5, 5)]
--- 조준형/w4_최소힙.py
+++ 조준형/w4_최소힙.py
@@ -1,13 +1,20 @@
-#지금 내가 해야 되는 것은 힙을 사용하는 코드이다. 
-# 내가 힙을 사용할 수 있나? 
-#내가 해야 할 것: 힙을 사용하는 법을 이번에 찬찬히 살펴본다.
-
-import sys
 import heapq
+import sys  #파이썬 인터프리터를 제어할 수 있는 방법을 제공한다. 
 
-heap = []
-heapq.heappush(heap, 50)
-heapq.heappush(heap, 10)
-heapq.heappush(heap, 20)
+input = sys.stdin.readline
+#한 두줄 입력받는 문제들과는 다르게, 반복문으로 여러줄을 입력받아야 할 때는 이렇게 해준다.
 
-print(heap)
+#파이썬 heapq를 사용하면 기본적으로 최소 힙이다. 
+min_heap = []
+
+for _ in range(int(input())):
+    n = int(input())
+
+    if n==0:
+        if len(min_heap):
+            print(heapq.heappop(min_heap))
+        else:
+            print(0)
+
+    else: 
+        heapq.heappush(min_heap, n)
