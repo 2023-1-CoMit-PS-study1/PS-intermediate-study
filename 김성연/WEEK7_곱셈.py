ed66faaa5c9791a00bd80b660c1d11d85a68eec0
Seongyeon Kim <127931696+segfault10@users.noreply.github.com>
2023-05-02 17:25:16 +0900

    Add files via upload

---
+++ 김성연/WEEK7_곱셈.py
@@ -0,0 +1,13 @@
+import sys
+def power(a, b):
+    if b == 0:
+        return 1
+    elif b%2 == 0:
+        t = power(a, b//2)
+        return t**2 % z
+    else:
+        return power(a, b-1) * a
+
+x,y,z = map(int,sys.stdin.readline().rstrip().split())
+
+print(power(x,y) % z)
