8c218098c718567230cdfdfb0684aa30ed9984b4
junhyeong <asdfg3128@gmail.com>
2023-07-08 22:18:34 +0900

    Update boj2178_미로탐색.py

--- 조준형/boj2178_미로탐색.py
+++ 조준형/boj2178_미로탐색.py
@@ -2,44 +2,42 @@ from collections import deque
 
 
 def maze_bfs(n,m,grid):
-    count = 0
+    shortest_path_len = -1
     row = n
     col = m
     visited = [[False]*col for _ in range(row)]
 
     
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+    dr = [-1, 1, 0, 0]
+    dc = [0, 0, -1, 1]
     visited[0][0] = True
     queue = deque()
-    queue.append((0,0))
-    count += 1
+    queue.append((0,0, 1))
+    
     while queue:
-        cur_x, cur_y = queue.popleft()
+        cur_r, cur_c, cur_len = queue.popleft()
 
-        
+        if cur_r == row -1 and cur_c == col-1:
+            shortest_path_len = cur_len
+            break
+
         for i in range(4):
-            next_x = cur_x + dx[i]
-            next_y = cur_y + dy[i]
-            if next_x >= 0 and next_x < col and next_y >= 0 and next_y < row:
-                if grid[next_x][next_y] == 1 and not visited[next_x][next_y]:
-                    queue.append((next_x,next_y))
-                    visited[next_x][next_y] = True
+            next_r = cur_r + dr[i]
+            next_c = cur_c + dc[i]
+            if next_r >= 0 and next_r < row and next_c >= 0 and next_c < col:
+                if grid[next_r][next_c] == 1 and not visited[next_r][next_c]:
+                    queue.append((next_r,next_c, cur_len+1))
+                    visited[next_r][next_c] = True
+                    
+    return shortest_path_len
 
     
-
-
-
-
-
-
-
-
-
-
+                    
 
 
 if __name__ == '__main__':
 
     n, m = map(int, input().split())
-    grid = [list(map(int, input().split())) for _ in range(n)]
+    grid = [list(map(int, list(input().strip()))) for _ in range(n)]
+
+    print(maze_bfs(n,m,grid))
