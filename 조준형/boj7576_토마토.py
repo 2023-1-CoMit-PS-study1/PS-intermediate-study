09cf79712b5ed8708be76b02fe19933e6c3e5510
junhyeong <asdfg3128@gmail.com>
2023-08-07 17:50:42 +0900

    Update boj7576_토마토.py

--- 조준형/boj7576_토마토.py
+++ 조준형/boj7576_토마토.py
@@ -2,10 +2,38 @@ from collections import deque
 
 def tomato_bfs(row,col,grid):
 
+    day = 0
+    #visited = [[False]*col for _ in range(row)]
 
-    visited = [[False]*col for _ in range(row)]
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    queue = deque()
+    
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == 1:
+                queue.append((i,j,0))
 
-    pass
+    while queue:
+        cur_x, cur_y, cur_day = queue.popleft()
+
+        if day < cur_day:
+            day = cur_day
+        
+        for i in range(4):
+            next_x = cur_x + dx[i]
+            next_y = cur_y + dy[i]
+            if next_x >= 0 and next_x < row and next_y >= 0 and next_y < col:
+                if grid[next_x][next_y] == 0:
+                    queue.append((next_x,next_y,cur_day+1))
+                    grid[next_x][next_y] = 1
+
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == 0:
+                return -1
+
+    return day
 
 
 #main
